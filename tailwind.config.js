9d13bfd9f6768797b96b9e7b8470a400f7c1d9c0
jbialkin98 <jbialkin98@gmail.com>
2023-02-20 01:17:05 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Bodoni: ['Bodoni Moda'],
+        Josefin: ['Josefin Sans'],
+        Quattrocento: ['Quattrocento'],
+      },
+    },
+  },
+  plugins: [],
+}
